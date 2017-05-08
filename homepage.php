501221ad160859dc984dcd37cd6a301007473ac3
Mantas Petrauskas <mantas.petrauskas96@gmail.com>
2017-05-08 13:37:50 +0300

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <h1>Hello. This is my home page.</h1>
+        <?php
+         session_start();
+         $name=$_SESSION['name'];    
+         echo'welcome: '. $name[1].'<br>';
+         echo'<a href="php/sign_out.php">Signout</a>';
+         ?>
+    </body>
+</html>
